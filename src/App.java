1f13a76563df4a995ea6b84254bd660bdc85da09
Alberto Rodrigues <allio.professional@gmail.com>
2024-04-25 17:13:12 -0300

    feat:testando leitor

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Leitor leitor = new Leitor("public//compras.csv");
+        leitor.lerCompras();
     }
 }
