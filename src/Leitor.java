423a75c9675cbb958c6509b65b6c90aef9c7a691
Alberto Rodrigues <allio.professional@gmail.com>
2024-04-25 17:11:01 -0300

    feat: criando classe leitor

--- src/Leitor.java
+++ src/Leitor.java
@@ -1,3 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leitor {
-    
+    private String arquivo;
+
+    public Leitor(String caminhoArquivo) {
+        this.arquivo = caminhoArquivo;
+    }
+
+    public List<Compra> lerCompras() {
+        List<Compra> compras = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
+            String linha;
+
+            while ((linha = reader.readLine()) != null) {
+                String[] entrada = linha.split(",");
+                int idPedido = Integer.parseInt(entrada[0]);
+                double valorPedido = Double.parseDouble(entrada[1]);
+                int anoPedido = Integer.parseInt(entrada[2]);
+                compras.add(new Compra(idPedido, valorPedido, anoPedido));
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao abrir o arquivo: " + arquivo);
+            e.printStackTrace();
+        }
+
+        return compras;
+    }
 }
+
